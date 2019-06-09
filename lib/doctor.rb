87607458a020a075a70fae4a30a1a01d6d3359a4
Justin Hutsell <hutselljustin@gmail.com>
2019-06-09 22:04:48 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,61 +1,28 @@
-# class Doctor 
-#   attr_reader :name
+class Doctor 
+  attr_reader :name
   
-#   @@all = []
+  @@all = []
   
-#   def initialize(name)
-#     @name = name 
+  def initialize(name)
+    @name = name 
     
-#     @@all << self
-#   end
+    @@all << self
+  end
   
-#   def new_appointment(date, patient)
-#     Appointment.new(date, patient, self)
-#   end
+  def new_appointment(date, patient)
+    Appointment.new(date, patient, self)
+  end
   
-#   def appointments 
-#     Appointment.all.select { |appointment| appointment.doctor == self } 
-#   end
+  def appointments 
+    Appointment.all.select { |appointment| appointment.doctor == self } 
+  end
   
-#   def patients 
-#     appointments.map { |appointment| appointment.patient }
-#   end
+  def patients 
+    appointments.map { |appointment| appointment.patient }
+  end
   
-#   def self.all 
-#     @@all 
-#   end
+  def self.all 
+    @@all 
+  end
   
-# end
-
-
-
-
-
-
-class Doctor
-    @@all=[]
-    attr_accessor :name
-
-    def initialize(name)
-        @name=name
-        @@all<< self
-    end
-
-    def self.all
-        @@all
-    end
-
-    def new_appointment(patient,date)
-      Appointment.new(date,patient,self)
-
-    end
-
-
-    def appointments
-        Appointment.all.select{|appointment_instance| appointment_instance.doctor==self}
-    end
-
-    def patients
-        self.appointments.map{|instance| instance.patient}
-    end
 end
