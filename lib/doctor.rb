bc85aca9b3e6d8eccb1d1455b2121103fe4554fa
Justin Hutsell <hutselljustin@gmail.com>
2019-06-09 22:04:03 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,30 +1,28 @@
-class Doctor 
-  attr_reader :name
+# class Doctor 
+#   attr_reader :name
   
-  @@all = []
+#   @@all = []
   
-  def initialize(name)
-    @name = name 
+#   def initialize(name)
+#     @name = name 
     
-    @@all << self
-  end
+#     @@all << self
+#   end
   
-  def new_appointment(date, patient)
-    Appointment.new(date, patient, self)
-  end
+#   def new_appointment(date, patient)
+#     Appointment.new(date, patient, self)
+#   end
   
-  def appointments 
-    Appointment.all.select { |appointment| appointment.doctor == self } 
-  end
+#   def appointments 
+#     Appointment.all.select { |appointment| appointment.doctor == self } 
+#   end
   
-  def patients 
-    appointments.map { |appointment| appointment.patient }
-  end
+#   def patients 
+#     appointments.map { |appointment| appointment.patient }
+#   end
   
-  def self.all 
-    @@all 
-  end
+#   def self.all 
+#     @@all 
+#   end
   
-  
-  
-end
+# end
