0691cb46e4d2feb0bf69ca27179f80db3dbec5cd
Justin Hutsell <hutselljustin@gmail.com>
2019-06-09 22:02:53 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,18 +1,18 @@
-class Appointment 
-  attr_accessor :date, :patient, :doctor 
+# class Appointment 
+#   attr_accessor :date, :patient, :doctor 
   
-  @@all = []
+#   @@all = []
   
-  def initialize(date, patient, doctor)
-    @date = date 
-    @patient = patient 
-    @doctor = doctor 
+#   def initialize(date, patient, doctor)
+#     @date = date 
+#     @patient = patient 
+#     @doctor = doctor 
     
-    @@all << self 
-  end
+#     @@all << self 
+#   end
   
-  def self.all 
-    @@all 
-  end
+#   def self.all 
+#     @@all 
+#   end
   
-end
+# end
